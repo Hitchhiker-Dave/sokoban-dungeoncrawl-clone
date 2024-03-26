61686d4f3fc8c23b5472873278f98839b5e7519b
Hitchhiker-Dave <mrhitchhiker42@gmail.com>
2024-03-26 00:17:17 -0700

    Art Update
    Added "better" looking art assets and made the movement less jank.
    
    To do: look up raycasting for manual collision checks

--- player.gd
+++ player.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+var move_distance = 32
+var tween: Tween
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -7,14 +9,30 @@ func _ready():
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _physics_process(delta):
 	# basic "tile movement"
 	# for some reason the analogue controls are inverted for the left stick
-	if (Input.is_action_just_released("Move Up")):
-		position.y = position.y - 50
-	if (Input.is_action_just_released("Move Down")):
-		position.y = position.y + 50
-	if (Input.is_action_just_released("Move Left")):
-		position.x = position.x - 50
-	if (Input.is_action_just_released("Move Right")):
-		position.x = position.x + 50
+	#convert to vector movement; should work fine if I keep the update on a key release
+	
+	if (Input.is_action_just_pressed("Move Up")):
+		move(Vector2.UP)
+	if (Input.is_action_just_pressed("Move Down")):
+		move(Vector2.DOWN)
+	if (Input.is_action_just_pressed("Move Left")):
+		move(Vector2.LEFT)
+	if (Input.is_action_just_pressed("Move Right")):
+		move(Vector2.RIGHT)
+
+func move(direction: Vector2):
+	
+	#movement and animation
+	var target_position = global_position + direction * move_distance
+	
+	#implement look ahead here; for collisions and interactables
+	#look up raycasting 
+	
+	if tween and tween.is_running():
+		return
+	tween = get_tree().create_tween()
+	tween.tween_property(self, "global_position", target_position, 0.16).set_trans(Tween.TRANS_BOUNCE)
+	
