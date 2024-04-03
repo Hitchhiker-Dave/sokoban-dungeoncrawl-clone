260ccf57f8fa9193b01a30e8d6e0aa52e78e541d
Hitchhiker-Dave <112595100+Hitchhiker-Dave@users.noreply.github.com>
2024-04-02 17:54:59 -0700

    Basic Collisions
    Added:
    Rudimentary Collision Detection
    Boulder Object w/ Rigidbody Collider
    
    To Do:
    Replace Current Raycast system with a Raycast2D node system; current system can't actually get the object it's pointing to

--- player.gd
+++ player.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 var move_distance = 32
 var tween: Tween
+@onready var rigid_body_2d = $RigidBody2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -29,8 +30,32 @@ func move(direction: Vector2):
 	var target_position = global_position + direction * move_distance
 	
 	#implement look ahead here; for collisions and interactables
-	#look up raycasting 
+	#bootleg collision using raytracing; currently prevents any movement if there is a collision
+	#gonna have to replace this with an actual raycast node/object since this doesn't actually get the object data
 	
+	var space_state = get_world_2d().direct_space_state
+	# use global coordinates, not local to node
+	var query = PhysicsRayQueryParameters2D.create(global_position, target_position, 
+													rigid_body_2d.get_collision_mask(), [self])
+	var result = space_state.intersect_ray(query)
+	#Collision Notes
+	#result.collider.name = "ObjectNameHere"
+	#tiles with colllision = "Tilemap"
+
+	
+	if result: #check if raycast actually hit something
+		
+		print(result.collider.name)
+		print(result.position)
+		#Wall Handling
+		if result.collider.name == "TileMap":
+			print("Hit Wall: ", result.position) 
+			return
+		elif result.collider.name == "RockCollision":
+			print("Hit Rock")
+			#get the object at the specific coords from result.position
+			#may need to get a new raycast just for this
+
 	if tween and tween.is_running():
 		return
 	tween = get_tree().create_tween()
