8eef575cc7bb056e122a4bd30e427d9f187389f4
Hitchhiker-Dave <112595100+Hitchhiker-Dave@users.noreply.github.com>
2024-04-18 14:57:23 -0700

    Generic Level Template
    Added Generic Level Template with built in level switch for streamlined level creation and implementation.
    
    Fixed:
    Added Capability to swap the next and previous player

--- Scripts/Player Handler.gd
+++ Scripts/Player Handler.gd
@@ -5,12 +5,13 @@ extends Node2D
 @onready var player_index = 0
 @onready var player_count : int
 @onready var player_reached_level_transition = false
-signal reached_next_level
+signal ready_for_next_level
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	active_player = player_list[player_index]
 	active_player.is_active = true
+	player_count = player_list.size()
 	
 	for i in range(player_list.size()):
 		get_node(player_list[i].to_string()).hit_level_transition.connect(_handle_player_leaving)
@@ -20,12 +21,21 @@ func _process(_delta):
 	if player_count <= 0: #no more players, handle win lose state
 		if player_reached_level_transition:
 			print("Send Signal to Transition to Next Level (Player Handler)")
-			reached_next_level.emit() 
+			ready_for_next_level.emit() 
 
 	if Input.is_action_just_pressed("Next Character"):
 		player_index += 1
 		if player_index > (player_list.size() - 1):
 			player_index = 0
+		
+		active_player.is_active = false
+		active_player = player_list[player_index]
+		active_player.is_active = true
+		
+	elif Input.is_action_just_pressed("Previous Character"):
+		player_index -= 1
+		if player_index <= -1:
+			player_index = player_count - 1
 		
 		active_player.is_active = false
 		active_player = player_list[player_index]
--- Scripts/level_handler.gd
+++ Scripts/level_handler.gd
@@ -6,13 +6,14 @@ var level_select = "res://Environments/Menus/level_select.tscn"
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var player_handler = get_node("Player Handler")
-	player_handler.reached_next_level.connect(_go_to_next_level)
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
 
-func _go_to_next_level():
-	print("Signal to Start Transition to the Next Level (Current Level)")
+func _on_level_select_button_pressed():
+	SceneSwitcher.switch_scene(level_select)
+
+func _on_player_handler_ready_for_next_level():
 	SceneSwitcher.switch_scene(next_level)
